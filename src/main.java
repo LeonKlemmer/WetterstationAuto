9c4a08b55bdea7e7201b5cde137886cd508611bf
Mankelmank <57710777+Mankelmank@users.noreply.github.com>
2021-04-20 14:14:33 +0200

    adding class GUI

--- src/main.java
+++ src/main.java
@@ -1,9 +1,5 @@
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Leon is here");
 
-        System.out.println("Jan was here");
-        System.out.println("Leon hat einen kleinen Penis");
     }
 }
