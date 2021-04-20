1bfca865cc253478c20ddd9d751ad8e1180674c9
Mankelmank <57710777+Mankelmank@users.noreply.github.com>
2021-04-20 16:47:29 +0200

    added Widges

--- src/GUI.java
+++ src/GUI.java
@@ -17,19 +17,54 @@ public class GUI {
         var jf = new JFrame();
         jf.setSize(width, height);
         jf.setTitle("Wetterstation");
-        jf.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jf.setLayout(null);
         jf.setResizable(false);
-
         jf.setLocationRelativeTo(null);
 
 
-        textfield = new JTextField();
-        textfield.setBounds(50, 25, 250, 30);
-        textfield.setEditable(false);
-        textfield.setText("Standort: ");
-        jf.add(textfield);
 
+        //Standort
+        textLocation = new JTextField();
+        textLocation.setBounds(50, 25, 250, 30);
+        textLocation.setEditable(false);
+        textLocation.setText("Standort: ");
+        jf.add(textLocation);
+
+        //Datum
+        textDate = new JTextField();
+        textDate.setBounds(50, 65, 250, 30);
+        textDate.setEditable(false);
+        textDate.setText("Datum: ");
+        jf.add(textDate);
+
+        //Uhrzeit
+        textTime = new JTextField();
+        textTime.setBounds(50, 105, 250, 30);
+        textTime.setEditable(false);
+        textTime.setText("Uhrzeit: ");
+        jf.add(textTime);
+
+        //Temperatur
+        textTemperature = new JTextField();
+        textTemperature.setBounds(50, 145, 250, 30);
+        textTemperature.setEditable(false);
+        textTemperature.setText("Temperatur: ");
+        jf.add(textTemperature);
+
+        //MonatDurchschnitt
+        textTemperatureMonth = new JTextField();
+        textTemperatureMonth.setBounds(50, 185, 250, 30);
+        textTemperatureMonth.setEditable(false);
+        textTemperatureMonth.setText("MonatsDurchschnitt: ");
+        jf.add(textTemperatureMonth);
+
+        //JahresDurchschnitt
+        textTemperatureYear = new JTextField();
+        textTemperatureYear.setBounds(50, 185, 250, 30);
+        textTemperatureYear.setEditable(false);
+        textTemperatureYear.setText("MonatsDurchschnitt: ");
+        jf.add(textTemperatureYear);
 
         jf.setVisible(true);
 
