e61e000e627db0877509df3e706357f23071afaf
Mankelmank <57710777+Mankelmank@users.noreply.github.com>
2021-04-20 10:46:06 +0200

    Jan Update

--- src/main.java
+++ src/main.java
@@ -2,5 +2,7 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Leon was here");
+
+        System.out.println("Jan was here");
     }
 }
