74f0f903b1af91124b82d7f19e23cd2f8dc8b23d
LeonKlemmer <64715493+LeonKlemmer@users.noreply.github.com>
2021-04-20 14:07:13 +0200

    Update cloud.java

--- src/cloud.java
+++ src/cloud.java
@@ -1,10 +1,40 @@
 public class cloud {
+    private String location;
+    private int temorature;
+    private String time;
+    private String date;
 
-    int temorature;
+    public String getLocation() {
+        return location;
+    }
 
-    String data;
-    int highestTemorature;
-    int lowestTemorature;
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getTemorature() {
+        return temorature;
+    }
+
+    public void setTemorature(int temorature) {
+        this.temorature = temorature;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
 
 
 }
