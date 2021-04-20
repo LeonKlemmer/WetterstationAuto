ee577d9eae2bed048da007d39c9865b631465862
LeonKlemmer <64715493+LeonKlemmer@users.noreply.github.com>
2021-04-21 00:18:31 +0200

    Sort with errors

--- src/ShowParameter.java
+++ src/ShowParameter.java
@@ -11,15 +11,15 @@ public class ShowParameter {
         Average average = new Average(data.clouds);
         HighLow highLow = new HighLow(data.clouds);
 
-        //data.csvConverter("C:\\Users\\klemm\\IdeaProjects\\WetterstationAuto\\Data\\Wetter.csv"); //generate list with Cloud objekts where data is stored in
-        data.csvConverter("C:\\Irgendwie\\Development\\WetterstationAuto\\Data\\Wetter.csv");
+        data.csvConverter("C:\\Users\\klemm\\IdeaProjects\\WetterstationAuto\\Data\\Wetter.csv"); //generate list with Cloud objekts where data is stored in
+        //data.csvConverter("C:\\Irgendwie\\Development\\WetterstationAuto\\Data\\Wetter.csv");
         System.out.println("Average : "+average.getAverage());// get average temp
         System.out.println("Highest : "+highLow.getHighestTemp().getTemprature());
         System.out.println("Lowest : "+highLow.getLowestTemp().getTemprature());
 
 
         for(int i = 0; i <= data.clouds.size(); i++){
-            new GUI(data.clouds.get(i).getLocation(), data.clouds.get(i).getDate(), data.clouds.get(i).getTime(), data.clouds.get(i).getTemprature(), "buy full version", "buy full version");
+             new GUI(data.clouds.get(i).getLocation(), data.clouds.get(i).getDate(), data.clouds.get(i).getTime(), data.clouds.get(i).getTemprature(), "buy full version", "buy full version");
 
             TimeUnit.SECONDS.sleep(3);
         }
---
+++ src/Sort.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class Sort {
+    public ArrayList<Cloud> list;
+ public void Sort(ArrayList<Cloud> list){
+
+ }
+ public ArrayList<Cloud> getSortedListHigh(){
+     int b,k,tmp;
+     for (k=1; k < zahlen; k++){
+         for(b = 0; b < (zahlen-k); b++){
+             if(liste[b] > liste[b+1]) {
+                 tmp=liste[b+1];
+                 liste[b+1]=liste[b];
+                 liste[b]=tmp;
+             }
+         }
+     }
+     return
+ }
+
+
+
+}
