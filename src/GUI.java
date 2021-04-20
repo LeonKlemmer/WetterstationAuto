25412b8ba2b64bc83cf67b560fb9e5d0c4a1ab36
LeonKlemmer <64715493+LeonKlemmer@users.noreply.github.com>
2021-04-21 00:20:49 +0200

    Revert "Merge branch 'master' of https://github.com/LeonKlemmer/WetterstationAuto"
    This reverts commit 044885327c52f23b1d6703b12f3f7e7a14bddd40, reversing
    changes made to ee577d9eae2bed048da007d39c9865b631465862.

--- src/GUI.java
+++ src/GUI.java
@@ -14,27 +14,24 @@ public class GUI {
     JLabel textTemperature;
     JLabel textTemperatureMonth;
     JLabel textTemperatureYear;
-    boolean test = true;
 
 
+    public GUI(String location, String date, String time, String temperature, String temperatureMonth, String temperaturYear) {
 
-    public GUI() {
-
-            var jf = new JFrame();
-            jf.setSize(width, height);
-            jf.setTitle("Wetterstation");
-            jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            jf.setLayout(null);
-            jf.setResizable(false);
-            jf.setLocationRelativeTo(null);
-            jf.getContentPane().setBackground(Color.DARK_GRAY);
-
+        var jf = new JFrame();
+        jf.setSize(width, height);
+        jf.setTitle("Wetterstation");
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jf.setLayout(null);
+        jf.setResizable(false);
+        jf.setLocationRelativeTo(null);
+        jf.getContentPane().setBackground(Color.DARK_GRAY);
 
 
         //Standort
         textLocation = new JLabel();
         textLocation.setBounds(50, 25, 250, 30);
-        textLocation.setText("Standort: ");
+        textLocation.setText("Standort: " + location);
         textLocation.setFont(new Font("Calabri", Font.ITALIC,16));
         textLocation.setForeground(Color.LIGHT_GRAY);
         jf.add(textLocation);
@@ -44,13 +41,13 @@ public class GUI {
         textDate.setBounds(50, 65, 250, 30);
         textDate.setFont(new Font("Calabri", Font.ITALIC,16));
         textDate.setForeground(Color.LIGHT_GRAY);
-        textDate.setText("Datum: ");
+        textDate.setText("Datum: " + date);
         jf.add(textDate);
 
         //Uhrzeit
         textTime = new JLabel();
         textTime.setBounds(50, 105, 250, 30);
-        textTime.setText("Uhrzeit: ");
+        textTime.setText("Uhrzeit: " + time);
         textTime.setFont(new Font("Calabri", Font.ITALIC,16));
         textTime.setForeground(Color.LIGHT_GRAY);
         jf.add(textTime);
@@ -58,7 +55,7 @@ public class GUI {
         //Temperatur
         textTemperature = new JLabel();
         textTemperature.setBounds(50, 145, 250, 30);
-        textTemperature.setText("Temperatur: ");
+        textTemperature.setText("Temperatur: " + temperature + "°");
         textTemperature.setFont(new Font("Calabri", Font.ITALIC,16));
         textTemperature.setForeground(Color.LIGHT_GRAY);
         jf.add(textTemperature);
@@ -66,7 +63,7 @@ public class GUI {
         //MonatDurchschnitt
         textTemperatureMonth = new JLabel();
         textTemperatureMonth.setBounds(50, 185, 250, 30);
-        textTemperatureMonth.setText("Monatsdurchschnitt: ");
+        textTemperatureMonth.setText("Monatsdurchschnitt: " + temperatureMonth);
         textTemperatureMonth.setFont(new Font("Calabri", Font.ITALIC,16));
         textTemperatureMonth.setForeground(Color.LIGHT_GRAY);
         jf.add(textTemperatureMonth);
@@ -74,7 +71,7 @@ public class GUI {
         //JahresDurchschnitt
         textTemperatureYear = new JLabel();
         textTemperatureYear.setBounds(50, 300, 250, 30);
-        textTemperatureYear.setText("Jahresdurchschnitt: ");
+        textTemperatureYear.setText("Jahresdurchschnitt: " + temperaturYear);
         textTemperatureYear.setFont(new Font("Calabri", Font.ITALIC,16));
         textTemperatureYear.setForeground(Color.LIGHT_GRAY);
         jf.add(textTemperatureYear);
@@ -83,15 +80,8 @@ public class GUI {
 
     }
 
+    public void showData(){
 
-        //überschreibung der werte
-    public void setGuiText(String location, String date, String time, String temperature, String temperatureMonth, String temperaturYear){
-        textLocation.setText("Standort: " + location);
-        textDate.setText("Datum: " + date);
-        textTime.setText("Uhrzeit: " + time);
-        textTemperature.setText("Temperatur: " + temperature + "°");
-        textTemperatureMonth.setText("Monatsdurchschnitt: " + temperatureMonth);
-        textTemperatureYear.setText("Jahresdurchschnitt: " + temperaturYear);
     }
 
 }
