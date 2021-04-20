2d8edf7facc339de3248e960831252b5aa9a2494
Mankelmank <57710777+Mankelmank@users.noreply.github.com>
2021-04-20 16:22:12 +0200

    simple GUI

--- src/GUI.java
+++ src/GUI.java
@@ -1,3 +1,40 @@
+import javax.swing.*;
+
 public class GUI {
+
+    int height = 600;
+    int width = 350;
+
+    JTextField textLocation;
+    JTextField textDate;
+    JTextField textTime;
+    JTextField textTemperature;
+    JTextField textTemperatureMonth;
+    JTextField textTemperatureJear;
+
+    public GUI() {
+
+        var jf = new JFrame();
+        jf.setSize(width, height);
+        jf.setTitle("Wetterstation");
+        jf.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        jf.setLayout(null);
+        jf.setResizable(false);
+
+        jf.setLocationRelativeTo(null);
+
+
+        textfield = new JTextField();
+        textfield.setBounds(50, 25, 250, 30);
+        textfield.setEditable(false);
+        textfield.setText("Standort: ");
+        jf.add(textfield);
+
+
+        jf.setVisible(true);
+
+    }
+
+
 
 }
--- src/main.java
+++ src/main.java
@@ -1,5 +1,6 @@
 public class main {
     public static void main(String[] args) {
+        GUI gui = new GUI();
 
     }
 }
