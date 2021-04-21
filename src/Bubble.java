d14a2b8960d6d265c624cf42f3ab47d38d049242
LeonKlemmer <64715493+LeonKlemmer@users.noreply.github.com>
2021-04-21 18:38:14 +0200

    Bubblesort implementation
    bug fixes

---
+++ src/Bubble.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+
+public class Bubble {
+    private ArrayList<Cloud> bubbleClouds = new ArrayList<Cloud>();
+
+    public Bubble(ArrayList<Cloud> bubbleClouds) {
+        this.bubbleClouds = bubbleClouds;
+    }
+
+    public ArrayList<Cloud> bubblesortLowest() {
+        Cloud temp;
+        for(int i=1; i<bubbleClouds.size(); i++) {
+            for(int j=0; j<bubbleClouds.size()-i; j++) {
+                if((int)(Long.parseLong(bubbleClouds.get(j).getTemprature()))>(int)(Long.parseLong(bubbleClouds.get(j+1).getTemprature()))) {
+                    temp=bubbleClouds.get(j);
+                    bubbleClouds.set(j,bubbleClouds.get(j+1));
+                    bubbleClouds.set(j+1,temp);
+                }
+
+            }
+
+
+    }for(int g=0; g<bubbleClouds.size(); g++) {
+            System.out.println(g);
+            System.out.println("Bubble : "+bubbleClouds.get(g).getTemprature());
+        }
+        return bubbleClouds;
+}//niedrigste zuerst
+    public ArrayList<Cloud> bubblesortHighest() {
+        Cloud temp;
+        for(int i=1; i<bubbleClouds.size(); i++) {
+            for(int j=0; j<bubbleClouds.size()-i; j++) {
+                if((int)(Long.parseLong(bubbleClouds.get(j).getTemprature()))<(int)(Long.parseLong(bubbleClouds.get(j+1).getTemprature()))) {
+                    temp=bubbleClouds.get(j);
+                    bubbleClouds.set(j,bubbleClouds.get(j+1));
+                    bubbleClouds.set(j+1,temp);
+                }
+
+            }
+
+
+        }for(int g=0; g<bubbleClouds.size(); g++) {
+            System.out.println(g);
+            System.out.println("Bubble : "+bubbleClouds.get(g).getTemprature());
+        }
+        return bubbleClouds;
+    }//Höchste zuerst
+
+}
--- src/ShowParameter.java
+++ src/ShowParameter.java
@@ -10,14 +10,19 @@ public class ShowParameter {
         GetData data = new GetData();
         Average average = new Average(data.clouds);
         HighLow highLow = new HighLow(data.clouds);
+        Bubble bubble = new Bubble(data.clouds);
 
-        //data.csvConverter("C:\\Users\\klemm\\IdeaProjects\\WetterstationAuto\\Data\\Wetter.csv"); //generate list with Cloud objekts where data is stored in
-        data.csvConverter("C:\\Irgendwie\\Development\\WetterstationAuto\\Data\\Wetter.csv");
+
+        data.csvConverter("C:\\Users\\klemm\\IdeaProjects\\WetterstationAuto\\Data\\Wetter.csv"); //generate list with Cloud objekts where data is stored in
+        //data.csvConverter("C:\\Irgendwie\\Development\\WetterstationAuto\\Data\\Wetter.csv");
         System.out.println("Average : "+average.getAverage());// get average temp
         System.out.println("Highest : "+highLow.getHighestTemp().getTemprature());
         System.out.println("Lowest : "+highLow.getLowestTemp().getTemprature());
 
-
+        System.out.println("Bubble sort started Lowest ");
+        bubble.bubblesortLowest();
+        System.out.println("Bubble sort started Highest");
+        bubble.bubblesortHighest();
         for(int i = 0; i <= data.clouds.size(); i++){
             gui.setGuiText(data.clouds.get(i).getLocation(), data.clouds.get(i).getDate(), data.clouds.get(i).getTime(), data.clouds.get(i).getTemprature(), "buy full version", "buy full version");
 
